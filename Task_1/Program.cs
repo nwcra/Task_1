b66f152cd63022c202f90c9c959183d301bb5bfd
nwcra <artemova-1990@ya.ru>
2017-01-19 17:18:08 +0300

    task
    Test task

---
+++ Task_1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Sort function
+        /// </summary>
+        /// <param name="value">Unsorted dictonary</param>
+        /// <returns>Sorted dictonary</returns>
+        static public Dictionary<string, string> Sort (Dictionary<string, string> cards)
+        {
+            Dictionary<string, string> sortedcards = new Dictionary<string, string>();
+            string nextCity = null;
+
+            foreach (var c in cards)
+            {
+                if (!cards.ContainsValue(c.Key))
+                {
+                    sortedcards.Add(c.Key, c.Value);
+                    nextCity = c.Value;
+                    break;
+                }
+            }
+
+            while (cards.ContainsKey(nextCity))
+            {
+                sortedcards.Add(nextCity, cards[nextCity]);
+                nextCity = cards[nextCity];
+            }
+
+            return sortedcards;
+        }
+
+        /// <summary>
+        /// Enter point method
+        /// </summary>
+        static public void Main()
+        {
+            Dictionary<string, string> cards = new Dictionary<string, string>
+            {
+                   {"Кельн", "Москва"},
+                   {"Мельбурн", "Кельн"},
+                   {"Москва", "Париж"},
+
+            };
+
+            Dictionary<string, string> sortedcards = Sort(cards);
+
+            foreach (var c in sortedcards)
+            {
+                Console.WriteLine("{0} - {1}", c.Key, c.Value);
+            }
+
+        }
+    }
+}
---
+++ Task_1Test/Task_1Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Task_1;
+
+namespace Task_1Test
+{
+    [TestClass]
+    public class Task_1Test
+    {
+        public bool DictonaryEquals(Dictionary<string, string> firstDictonary, Dictionary<string, string> secondDictionary)
+        {
+
+            if (firstDictonary.Count != secondDictionary.Count)
+                return false;
+
+            bool result = firstDictonary.OrderBy(pair => pair.Key)
+               .SequenceEqual(secondDictionary.OrderBy(p => p.Key)) && firstDictonary.OrderBy(p => p.Value)
+               .SequenceEqual(secondDictionary.OrderBy(p => p.Value));
+
+            return result;
+        }
+
+        [TestMethod]
+        public void TestSortMethod()
+        {
+            //arrange
+            Dictionary<string, string> unsorted = new Dictionary<string, string>
+            { 
+                 {"Казань", "Орел"},
+                 {"Хабаровск", "Киров"},
+                 {"Питербург", "Хабаровск"},
+                 {"Орел", "Питербург"},
+                 {"Новгород", "Казань"},
+                 {"Москва", "Новгород"},
+            };
+
+            Dictionary<string, string> expected = new Dictionary<string, string>
+            { 
+                  {"Москва", "Новгород"},
+                  {"Новгород", "Казань"},
+                  {"Казань", "Орел"},
+                  {"Орел", "Питербург"},
+                  {"Питербург", "Хабаровск"},
+                  {"Хабаровск", "Киров"},
+            };
+            //act
+            Dictionary<string, string> actual = Program.Sort(unsorted);
+
+           
+            //assert
+ 
+            Assert.IsTrue(DictonaryEquals(expected, actual));
+        }
+    }
+}
